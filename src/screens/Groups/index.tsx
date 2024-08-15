a60c58277a6aa6f6cc0393dc98b51731f63f9b92
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-08-15 18:17:08 -0300

    feat: :rocket: Create  project

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import {  Text, View } from 'react-native';
+
+export  function Groups() {
+  return (
+    <View >
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+}
+
