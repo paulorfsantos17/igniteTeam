73847b6e0f6a8743aa173e1fd7acdc5bb49d1fc1
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-08-16 10:47:53 -0300

    chore: :wrench: add eslint and config

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,5 @@
-import { Container } from './styles';
+import { Container } from './styles'
 
-export  function Groups() {
-  return (
-    <Container>
-    </Container>
-  );
+export function Groups() {
+  return <Container></Container>
 }
-
