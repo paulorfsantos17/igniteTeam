c9c61a1a510f4ab4476341bf36d72ba46fc971e6
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-08-16 10:43:35 -0300

    feat: add Fontes Roboto and create Loading

--- App.tsx
+++ App.tsx
@@ -1,13 +1,24 @@
 import { Groups } from "@screens/Groups";
 import { ThemeProvider } from "styled-components";
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 
 import theme from './src/themes'
+import { ActivityIndicator } from "react-native";
+import { Loading } from "@components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+  
+  
+
   return (
   <>
   <ThemeProvider theme={theme}>
-  <Groups />
+    {
+      false
+      ? <Groups /> 
+      : <Loading />
+    }
   </ThemeProvider>
   </>
   );
---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,10 @@
+import { Container, LoadIndicator } from './style'
+
+
+export  function Loading() {
+  return (
+    <Container>
+      <LoadIndicator />
+    </Container>
+  )
+}
