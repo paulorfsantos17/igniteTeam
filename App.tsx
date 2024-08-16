6559659c4ab935dade50be261a36df155b388ea5
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-08-16 10:53:52 -0300

    feat: ensure statusBar is config correctly

--- App.tsx
+++ App.tsx
@@ -1,26 +1,29 @@
-import { Groups } from "@screens/Groups";
-import { ThemeProvider } from "styled-components";
-import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+/* eslint-disable camelcase */
+import { Groups } from '@screens/Groups'
+import { ThemeProvider } from 'styled-components'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
 
 import theme from './src/themes'
-import { ActivityIndicator } from "react-native";
-import { Loading } from "@components/Loading";
+import { Loading } from '@components/Loading'
+import { StatusBar } from 'react-native'
 
 export default function App() {
-  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
-  
-  
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-  <>
-  <ThemeProvider theme={theme}>
-    {
-      false
-      ? <Groups /> 
-      : <Loading />
-    }
-  </ThemeProvider>
-  </>
-  );
+    <>
+      <ThemeProvider theme={theme}>
+        <StatusBar
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
+        />
+        {fontsLoaded ? <Groups /> : <Loading />}
+      </ThemeProvider>
+    </>
+  )
 }
-
